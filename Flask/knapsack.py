977ade8180603153f3ec511919f3fca22a452ae9
anujuw080 <anuj.uw.080@gmail.com>
2017-10-12 12:12:14 -0600

    Update knapsack.py

--- Flask/knapsack.py
+++ Flask/knapsack.py
@@ -1,27 +1,26 @@
 import numpy as np
-def knapsack(n, K ):
-    W_n = [1, 3, 4, 5]
-    Val_n = [1, 4, 5, 7]
-    W_knapsack = np.empty([n, K], dtype="int")
-    for j in range(0, K):
-        W_knapsack[0][j] = 0
-    for i in range(1, n):
-        for j in range(0, K):
-            if (W_n[i] > j):
-                W_knapsack[i][j] = W_knapsack[i - 1][j]
-            elif ((Val_n[i] + W_knapsack[i - 1][j - W_n[i]] > W_knapsack[i - 1][j])):
-                W_knapsack[i][j] = Val_n[i] + W_knapsack[i - 1][j - W_n[i]]
-            else:
-                W_knapsack[i][j] = W_knapsack[i - 1][j]
 
-    print(list(W_knapsack))
-    return W_knapsack
+def knapsack(ln, K ,w):
+    W_n = list(map(int,K.split(' ')))
+    Val_n = list(map(int,ln.split(' ')))
+    n = len(W_n)
+    res=knapsackRec(w,W_n,Val_n,n)
+    return res 
 
 
-# def main():
-#     print(knapsack(4, 9))
 
-# main()
 
-# if __name__ == "__main__":
-#     main()
+def knapsackRec(W , wt , val , n):
+
+    # Base Case
+    if n == 0 or W == 0 :
+        return 0
+
+    # If weight of the nth item is more than Knapsack of capacity
+    # W, then this item cannot be included in the optimal solution
+    if (wt[n-1] > W):
+        return knapsackRec(W , wt , val , n-1)
+
+    else:
+        return max(val[n-1] + knapsackRec(W-wt[n-1] , wt , val , n-1),
+                   knapsackRec(W , wt , val , n-1))
